145c2134b9f016ffe4b699f80de0452b2521151b
nasiriCoder <nsamia210@gmail.com>
2025-12-23 01:36:11 +0100

    app msg changed

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello DevOps Student! CI/CD  is working."
+    return "Hello DevOps Student! CI/CD  is working after update."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
